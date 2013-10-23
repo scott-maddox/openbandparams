7ec4bb2a4484fa5c18023194effea290aaf28232
Scott Maddox <smaddox@utexas.edu>
2013-10-22 22:28:10 -0500

    added setup.py, and added some markdown headers to developers.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='openbandparams',
+      version='0.1',
+      description='open source semiconductor band parameters',
+      url='https://github.com/scott-maddox/openbandparams',
+      author='Scott Maddox',
+      author_email='smaddox@utexas.edu',
+      license='AGPLv3',
+      packages=['openbandparams'],
+      zip_safe=True)
