49f78020f93f8059f5db259c6f69450b30eac970
Scott Maddox <smaddox@utexas.edu>
2013-11-17 16:02:17 -0600

    added docstrings to algorithms module

--- src/openbandparams/algorithms.py
+++ src/openbandparams/algorithms.py
@@ -19,9 +19,35 @@
 ##############################################################################
 
 def sign(x):
+    """
+    Returns -1, 0, or 1 if `x` is negative, zero, or positive, respectively.
+    """
     return cmp(x, 0)
 
 def bisect(func, a, b, xtol=1e-12, maxiter=100):
+    """
+    Finds the root of `func` using the bisection method.
+    
+    Requirements
+    ------------
+    func must be continuous function that accepts a single number input
+        and returns a single number
+    `func(a)` and `func(b)` must have opposite sign
+    
+    Parameters
+    ----------
+    func : function
+        the function that we want to find the root of
+    a : number
+        one of the bounds on the input
+    b : number
+        the other bound on the input
+    xtol : number, optional
+        the solution tolerance of the input value. The algorithm is
+        considered converged if `abs(b-a)2. < xtol`
+    maxiter : number, optional
+        the maximum number of iterations allowed for convergence
+    """
     fa = func(a)
     if fa == 0.:
         return a
