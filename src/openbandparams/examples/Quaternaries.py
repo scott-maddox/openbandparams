43c208eee98f34178ce2894f7a928d26c19bc26f
Scott Maddox <smaddox@utexas.edu>
2014-12-20 01:47:55 -0600

    fixed examples not loading local openbandparams (for real this time)

--- src/openbandparams/examples/Quaternaries.py
+++ src/openbandparams/examples/Quaternaries.py
@@ -18,14 +18,11 @@
 #
 #############################################################################
 # Make sure we import the local openbandparams version
-try:
-    from openbandparams import *
-except ImportError:
-    import os
-    import sys
-    sys.path.append(
-        os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-    from openbandparams import *
+import os
+import sys
+sys.path.insert(0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from openbandparams import *
 
 
 # Some identities and inequalities
