a6dd9b742184eb6c3ed0e81f9b247c01a5c4a575
Scott Maddox <smaddox@utexas.edu>
2015-04-08 22:45:51 -0500

    add `examples/Strained_Parameters.py`

---
+++ src/openbandparams/examples/Strained_Parameters.py
@@ -0,0 +1,44 @@
+#
+#   Copyright (c) 2013-2015, Scott J Maddox
+#
+#   This file is part of openbandparams.
+#
+#   openbandparams is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   openbandparams is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with openbandparams.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+# Make sure we import the local openbandparams version
+import os
+import sys
+sys.path.insert(0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from openbandparams import *
+import string
+
+# Print all parameters of all strained III-V zinc blende alloys
+params = {}
+for binary in iii_v_zinc_blende_binaries:
+    for param in binary.strained_001(0).get_unique_parameters():
+        if param.name not in params:
+            params[param.name] = param
+names = [n for n,p in sorted(params.items())]
+descriptions = [p.description for n,p in sorted(params.items())]
+max_name_width = max([len(name) for name in names])
+max_desc_width = max([len(desc) for desc in descriptions])
+
+print '='*max_name_width+'   '+'='*max_desc_width
+print '{}   {}'.format(string.ljust('Parameter', max_name_width),'Description')
+print '='*max_name_width+'   '+'='*max_desc_width
+for name, desc in zip(names, descriptions):
+    print '{}   {}'.format(string.ljust(name, max_name_width),desc)
+print '='*max_name_width+'   '+'='*max_desc_width
